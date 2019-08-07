94f3b2f2788b4e2c9f3e81cb13954e4e183e2702
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-08-07 10:18:16 +0300

    Remove comments

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,8 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
 class App extends React.Component {
-  // state = {
-  //   total: null,
-  //   next: null,
-  //   operation: null,
-  // };
-
-  // handleClick = buttonName => this.setState(prevState => (
-  //   calculate(prevState, buttonName)
-  // ))
 
   render() {
-  // const { total, next } = this.state;
   return (
     <div className="calculator">
       <Display/>
